277c941903e93fc1b41b10e6d60ab89a4e6378a2
HOANG HUNG ANH <30067814@st.buv.edu.vn>
2026-08-19 09:10:45 +0700

    Admin.py updated & forgot_password make

--- apps/forgot_password/models.py
+++ apps/forgot_password/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.auth.models import User
 
 # Create your models here.
+class Forgot_password_request(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_forgot_password")
+    code = models.TextField()
+    expire = models.DateTimeField()
+    status = models.CharField(max_length=255)
--- apps/forgot_password/views.py
+++ apps/forgot_password/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django_ratelimit.decorators import ratelimit
+import time
+from .models import Forgot_password_request
+import random
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
+
+def load_email_html():
+    with open("/templates/bio/forgot_password/mail.html") as file:
+        return file.read()
+
+def code_generator():
+    code = random.randint(0,100000)
+    
+
+@ratelimit(key='ip', rate='10/m')
+def forgot_password(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+
+        user = User.objects.get(email=email)
+        if user:
+            forgot_request = Forgot_password_request.objects.create(
+                user=user,
+                code=
+            )
