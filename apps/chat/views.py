ebced93aa6f26eaabafa1e58267471f28766617f
Hoàng Hùng Anh <codedr123456789@gmail.com>
2026-08-21 13:15:18 +0700

    UI Chatroom maked

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django_ratelimit.decorators import ratelimit
 from django.db.models import Q
 from .models import *
@@ -138,5 +138,40 @@ def reply_image(request, receiver_id, img_id):
 @ratelimit(key='user_or_ip', rate='500/m')
 @login_required
 def room(request, room_id):
-    return render(request, "chat/room.html", {'room_id':room_id})
+    bio = Bio.objects.filter(user=request.user).first()
+    # Chỉ cho vào phòng mà chính mình là thành viên
+    chat_room = ChatRoom.objects.filter(
+        Q(user1=bio) | Q(user2=bio), id=room_id
+    ).select_related('user1__user', 'user2__user').first()
+    if not chat_room:
+        return redirect('chat_list')
+
+    receiver = chat_room.user2 if chat_room.user1_id == bio.id else chat_room.user1
+
+    chat_messages = list(
+        chat_room.messages_at_chat_room
+        .select_related('sender__user', 'message_reply__sender__user', 'image_reply')
+        .order_by('-created_at')[:50]
+    )[::-1]
+
+    if chat_room.user1_id == bio.id:
+        receiver_last_read_msg_id = chat_room.user2_last_read_msg_id
+        my_read_field = 'user1_last_read_msg_id'
+    else:
+        receiver_last_read_msg_id = chat_room.user1_last_read_msg_id
+        my_read_field = 'user2_last_read_msg_id'
+
+    # Đánh dấu mình đã đọc tới tin mới nhất (update_fields để không đụng updated_at)
+    if chat_messages and getattr(chat_room, my_read_field) < chat_messages[-1].id:
+        setattr(chat_room, my_read_field, chat_messages[-1].id)
+        chat_room.save(update_fields=[my_read_field])
+
+    return render(request, "chat/room.html", {
+        'room_id': room_id,
+        'my_bio_id': bio.id,
+        'receiver': receiver,
+        'receiver_user_id': receiver.user_id,
+        'receiver_last_read_msg_id': receiver_last_read_msg_id,
+        'chat_messages': chat_messages,
+    })
     
